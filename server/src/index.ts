433c33898c4c4945fa1b9395dc9387f349bbc4a1
hali031 <farahhalima@hotmail.com>
2025-01-03 10:01:05 -0500

    server modification

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,8 +40,8 @@ app.use("/teams", teamRoutes);
 
 /** SERVER */
 
-const port = process.env.PORT || 3000;
-app.listen(port,()=>{
+const port = Number(process.env.PORT) || 3000;
+app.listen(port,"0.0.0.0",()=>{
     console.log(`server running on port ${port}`)
 })
 
